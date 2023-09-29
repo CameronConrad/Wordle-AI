73209cf44c060fa1a94d0af93b21d909a8ba4143
CameronConrad <s1560576@student.mcckc.edu>
2023-09-29 09:50:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ class Solver:
         self.word_list = self.import_word_list()
 
         # Create a list of possible letters for each position
-        self.possible_letters = [
-            [[alphabet for i in range(5)] for j in range(5)],
-        ]
+        self.possible_letters = []
+        for i in range(5):
+            self.possible_letters.append([letter for letter in alphabet])
 
         # Create a list of necessary letters for the word
         # Each item in the list is a dictionary with the letter as the key and possible positions as the value
@@ -30,8 +30,7 @@ class Solver:
                 if not present:
                     self.necessary_letters.append({letter: [i]})
                 # Set possible letters to only the correct letter
-                for l in self.possible_letters:
-                    l[i] = [letter]
+                self.possible_letters[i] = [letter]
             # The letter is in the word, but not correct
             elif color_result[i] == 'y':
                 # Update necessary letters to remove the incorrect letter
@@ -40,31 +39,118 @@ class Solver:
                         l[letter].remove(i)
                         break
                 # Update possible letters to remove the incorrect letter
-                self.possible_letters[0][i].remove(letter)
+                self.possible_letters[i].remove(letter)
             # The letter is not in the word
             else:
                 # Update necessary letters to remove the incorrect letter
                 for l in self.necessary_letters:
                     if letter in l.keys() and len(l.values()) > 1:
                         l.pop(letter)
-                        break
                 # Update possible letters to remove the incorrect letter
-                self.possible_letters[0][i].remove(letter)
-            
+                for i, letters in enumerate(self.possible_letters):
+                    if letter in letters and len(letters) > 1:
+                        self.possible_letters[i].remove(letter)
+
     def update_word_list(self):
-        for word in self.word_list:
+       for word in self.word_list:
             # Check if the word is possible
             possible = True
             for i, letter in enumerate(word):
-                if letter not in self.possible_letters[0][i]:
+                if letter not in self.possible_letters[i]:
                     possible = False
                     break
+            if possible:
+                # Check if the word has the necessary letters
+                for l in self.necessary_letters:
+                    letter = l.keys()
+                    positions = l.values()
+                    # Check if the letter is in the word in one of the possible positions
+                    present = False
+                    for position in positions:
+                        if word[position[0]] == letter:
+                            present = True
+                            break
+                    if not present:
+                        possible = False
+                        break
             # Remove the word if it is not possible
             if not possible:
                 self.word_list.remove(word)
     
     def import_word_list(self):
         # Import the word list
-        with open('five-letter-words.txt', 'r') as f:
+        with open('fiveLetterWords.txt', 'r') as f:
             word_list = f.read().splitlines()
         return word_list
+
+
+class UserInterface:
+    def __init__(self):
+        self.solver = Solver()
+    
+    def clear_terminal(self):
+        # Clear the terminal
+        print("\033c", end="")
+
+    def get_input(self):
+        # Get input from the user
+        guess = input("Enter your guess: ")
+        color_result = input("Enter the color result (g: green, y: yellow, n: gray): ")
+
+        # Return the input
+        return guess, color_result
+
+    def update_solver(self, guess, color_result):
+        # Update the solver
+        self.solver.update_positions(guess, color_result)
+        self.solver.update_word_list()
+    
+    def print_word_list(self):
+        # Print the word list
+        for word in self.solver.word_list:
+            print(word)
+    
+    def get_input_for_restart(self):
+        # Ask the user if they want to restart
+        restart = input("Would you like to restart? (y/n): ").lower()
+        if restart == 'y':
+            return True
+        else:
+            return False
+    
+    def run(self):
+        # Run the program
+        while True:
+
+            # Get input from the user
+            guess, color_result = self.get_input()
+
+            # Update the solver
+            self.update_solver(guess, color_result)
+
+            # Clear the terminal
+            self.clear_terminal()
+
+            # Print the word list
+            self.print_word_list()
+
+            # Print the length of the word list
+            print("Number of words remaining:", len(self.solver.word_list))
+
+            # Print necessary letters
+            print("Necessary letters: {}".format(self.solver.necessary_letters))
+
+            # Print possible letters
+            print("Possible letters: {}".format(self.solver.possible_letters))
+
+            if len(self.solver.word_list) <= 1:
+                break
+        
+
+if __name__ == "__main__":
+    ui = UserInterface()
+    while True:
+        ui.run()
+        restart = ui.get_input_for_restart()
+        if not restart:
+            break
