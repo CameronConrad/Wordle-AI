1398180e7571fe20240dde1d74e97a96f5c46bb4
CameronConrad <s1560576@student.mcckc.edu>
2023-10-03 08:26:47 -0500

    Updated Wordle word list to include all valid words

--- wordList.py
+++ wordList.py
@@ -15,13 +15,20 @@ def sort_words():
         for word in word_list:
             f.write(word + '\n')
 
-def add_words(words: list):
+def add_words(*words: str):
     # Add words to the word list
     with open('fiveLetterWords.txt', 'a') as f:
         for word in words:
             f.write(word + '\n')
     # Sort the words
     sort_words()
+
+def format_words(*words: str):
+    # Format the words to be added to the word list
+    formatted_words = []
+    for word in words:
+        formatted_words.append(word.lower())
+    return formatted_words
 
 def main():
     pass
